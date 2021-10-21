ceff39d2894c612cb4355770e53305b30166b3dd
Matvey Zenin <Mattzenn@yandex.ru>
2021-10-21 15:51:43 +0300

    Подготовка + создание схемы и моделей ресурсов API

---
+++ models/movie.js
@@ -0,0 +1,72 @@
+const { Schema, model } = require('mongoose');
+const { isURL } = require('validator');
+
+const movieSchema = new Schema({
+  country: {
+    type: String,
+    require: true,
+  },
+  director: {
+    type: String,
+    require: true,
+  },
+  duration: {
+    type: Number,
+    require: true,
+  },
+  year: {
+    type: String,
+    require: true,
+  },
+  description: {
+    type: String,
+    require: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator(link) {
+        return isURL(link);
+      },
+    },
+  },
+  trailer: {
+    type: String,
+    required: true,
+    validate: {
+      validator(link) {
+        return isURL(link);
+      },
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator(link) {
+        return isURL(link);
+      },
+    },
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: Schema.Types.ObjectId,
+    ref: 'movie',
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    require: true,
+  },
+  nameEN: {
+    type: String,
+    require: true,
+  },
+});
+
+module.exports = model('movie', movieSchema);
---
+++ models/user.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+const { isEmail } = require('validator');
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(email) {
+        return isEmail(email);
+      },
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    unique: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    default: 'Новый пользователь',
+  },
+});
+
+module.exports = model('user', userSchema);
