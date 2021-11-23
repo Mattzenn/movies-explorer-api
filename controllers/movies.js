952914981c566b8de27f6ad75e0600de356b7b6a
Matvey Zenin <Mattzenn@yandex.ru>
2021-11-24 01:52:49 +0300

    доработка функционала

--- controllers/movies.js
+++ controllers/movies.js
@@ -4,7 +4,7 @@ const Forbidden = require('../errors/Forbidden');
 const BadRequest = require('../errors/BadRequest');
 
 const getMovies = (req, res, next) => {
-  Movie.find({})
+  Movie.find({ owner: req.user._id })
     .then((movies) => res.send(movies))
     .catch(next);
 };
